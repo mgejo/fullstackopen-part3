665d5edae13706ebc2a38e2ab018915453f84fee
Martin Gejo <66442848+mgejo@users.noreply.github.com>
2021-02-13 19:44:56 -0300

    uibuild

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
 
-app.use(express.static("build"));
+app.use(express.static("static"));
 app.use(cors());
 
 app.use(express.json());
