c14197c2d0f35bfe84730c3a1d0663dcac7e3cdf
Martin Gejo <66442848+mgejo@users.noreply.github.com>
2021-02-13 19:59:53 -0300

    asd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-
+//random comment
 app.use(express.static("static"));
 app.use(cors());
 
