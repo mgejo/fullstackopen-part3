6323e06a003c33b2ce931fc67053ed6ee41af375
Martin Gejo <66442848+mgejo@users.noreply.github.com>
2021-02-13 18:55:31 -0300

    create heroku app

--- index.js
+++ index.js
@@ -112,6 +112,6 @@ app.put("/api/persons/:id", (request, response) => {
   response.json(updatePerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
