d441327418dca5b511f4b123a4779dcf09ea0b8f
Martin Gejo <66442848+mgejo@users.noreply.github.com>
2021-02-16 17:23:18 -0300

    uibuild

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
   }
+  console.log("asdasdsadasd\n\n\n\nasdasdasadd");
   next(error);
 };
 
--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const uniqueValidator = require("mongoose-unique-validator");
 const url = process.env.MONGODB_URI;
 
 console.log("connecting to", url);
@@ -22,12 +22,14 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
   },
   number: {
     type: String,
     required: true,
   },
 });
+personSchema.plugin(uniqueValidator);
 
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
